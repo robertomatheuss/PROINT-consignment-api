fe33da8288a0e10a21d8ffd9624dd83ced66d5fe
Roberto Matheus <robertmatheusa@gmail.com>
2025-11-16 17:45:58 -0300

    add: config of sanctum

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+       
+    }
+    protected function configureRateLimiting(): void
+    {
+        
     }
 }

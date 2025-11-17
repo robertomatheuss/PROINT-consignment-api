fcdb01d5dbec12fe41097e32181c880ac87e1808
Roberto Matheus <robertmatheusa@gmail.com>
2025-11-17 01:37:33 -0300

    adjust config

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 // Grupo de rotas de autenticação
 Route::prefix('auth')->group(function () {
@@ -14,5 +15,8 @@ Route::prefix('auth')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
     });
+});
 
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::apiResource('users', UserController::class)->except(['create', 'edit']);
 });

cf43497724bc3c888973f987b46b45b3e4d07509
Roberto Matheus <robertmatheusa@gmail.com>
2025-11-20 21:56:50 -0300

    fix: routes

--- routes/api.php
+++ routes/api.php
@@ -4,19 +4,36 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TipoContratoController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\DocumentoController;
+    
 
 // Grupo de rotas de autenticação
 Route::prefix('auth')->group(function () {
 
-    // Login (público)
     Route::post('/login', [AuthController::class, 'login']);
 
-    // Rotas protegidas
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
     });
 });
 
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::apiResource('clientes', ClienteController::class)->except(['create', 'edit']);
+
+    Route::get('clientes/{cliente}/documentos', [DocumentoController::class, 'index']);
+    Route::post('clientes/{cliente}/documentos', [DocumentoController::class, 'store']);
+    Route::delete('documentos/{documento}', [DocumentoController::class, 'destroy']);
+    Route::put('documentos/{documento}/arquivo', [DocumentoController::class, 'updateFile']);
+
+});
+
 Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    Route::apiResource('users', UserController::class)->except(['create', 'edit']);
+    Route::apiResource('users', UserController::class)
+        ->except(['create', 'edit']);
+
+    Route::apiResource('tipos-contrato', TipoContratoController::class)
+        ->except(['create', 'edit']);
 });
